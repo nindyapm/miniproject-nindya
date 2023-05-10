7fe4d8ceefc59685414dae1fde95081a6cdcd3ed
Nindya Putri Maharani <nindyaputrimr@gmail.com>
2023-05-10 14:15:45 +0700

    modify

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,36 +1,56 @@
 import main from "../../assets/img/main.jpg"
 import { Link } from "react-router-dom"
+import content from "../../assets/img/ikon-landingpage.png"
 
 const Header = () => {
     return (
-        <section className="headers">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-6 col-lg-6">
-                        <div className="text-welcome" style={{fontSize:35, fontWeight:"bold"}}>
-                            Penampilan sempurna dengan tas rotan’s
-                        </div> 
-                        <div className="text-welcome-content">
-                            Tas Rotan karya masyarakat lokal asli Kabupaten Barito Timur. 
-                            Model tas yang menarik dan modern membuat penampilan anda semakin sempurna. 
-                            Melestarikan produk lokal pilihan terbaik.
+        <>
+            <section className="headers">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 col-lg-6">
+                            <div className="text-welcome" style={{fontSize:35, fontWeight:"bold"}}>
+                                Penampilan sempurna dengan tas rotan’s
+                            </div> 
+                            <div className="text-welcome-content">
+                                Tas Rotan karya masyarakat lokal asli Kabupaten Barito Timur. 
+                                Model tas yang menarik dan modern membuat penampilan anda semakin sempurna. 
+                                Melestarikan produk lokal pilihan terbaik.
+                            </div>
+                            <div className="buttons">
+                                <Link to={"/ProductPage"} className="btn">
+                                    Beli Sekarang
+                                </Link>
+                            </div>
                         </div>
-                        <div className="buttons">
-                            <Link to={"/ProductPage"} className="btn">
-                                Beli Sekarang
-                            </Link>
+                        <div className="col-md-6 col-lg-6">
+                            <img 
+                                className="w-50 main-foto"
+                                src={main}
+                                alt="Tas Rotan"
+                            />
                         </div>
                     </div>
-                    <div className="col-md-6 col-lg-6">
+                </div>
+            </section>
+
+            <section className="content">
+            <div className="container">
+                <div className="row">
+                    <p className="text-content">Kelebihan Tas Rotan’s</p>
+                </div>
+                <div className="row">
+                    <div style= {{ textAlign: "center", paddingTop: "10px"}}>
                         <img 
-                            className="w-50 main-foto"
-                            src={main}
-                            alt="Tas Rotan"
+                            className="w-50 "
+                            src={content}
+                            alt="Kelebihan Tas Rotan"
                         />
                     </div>
                 </div>
             </div>
         </section>
+        </>
     )
 }
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,5 @@
 import Navbar from "../components/navbar/Navbar"
 import Header from "../components/header/Header"
-import Content from "../components/content-landingpage/content"
 import FavProduct from "../components/products/product-landingpage"
 import Footer from "../components/footer/Footer"
 import "../assets/css/LandingPage.css"
@@ -10,7 +9,6 @@ const LandingPage = () => {
         <>
             <Navbar />
             <Header />
-            <Content />
             <FavProduct />
             <Footer />  
         </>
